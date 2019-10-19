fc032838d2414c397c20442f30ba71dea4689e21
Yura <yura.ozero@gmail.com>
2019-10-19 17:42:01 +0500

    массивы

---
+++ jopa2/Source.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream> 
+
+using namespace std;
+int main() {
+	setlocale(0, "");//Воспринимает русский текст
+
+	//for (итереируемый цикл)
+	//for (int i = 1; i <= 10; i++)  cout << i << endl;
+	//while (неитерируемый цикл)
+	//int i = 10;
+	//while (i > 5)
+	//{
+	//	i--;
+	//	cout << i << endl;
+	//}
+	//for(i;i>5;)
+	//{
+	//	i--;
+	//	cout << i << endl;
+	//}
+	////do while (нахуй не нужный)
+	//do
+	//{
+
+	//}while (i>5)
+
+	//int i = 0;
+
+	////for (i = 0 ; i <= 100; i++)
+	////	cout <<i<<" ";
+
+	//while (i < 100)
+	//{
+	//	++i;
+	//	cout << i << endl;
+	//}
+	//do
+	//{
+	//	++i;
+	//	if (i > 50) break;
+	//} while (i == 1);
+	//cout << i;
+		
+	//int arr[5] = {1,2,3,4,5};
+	//char str[4] = {'1','2','3','4'};//4 байт
+	//for (int i = 0; i < 4; i++) cout << str[i];
+	//string s = ""; //8 байт
+	//s = str;
+	//string s1 = "567890";
+
+	//cout << s+s1;
+	//int a;
+	//int arr[5];
+	//for(int i = 0;i<5;i++)
+	// cin >> arr[i];
+	//a = arr[0];
+	//for(int i = 0; i < 5; i++)
+	//	if (a > arr[i]) a = arr[i];
+
+	//cout << a << endl;
+	////найти минимальный из массива
+	system("pause");
+	return 0;
+}
